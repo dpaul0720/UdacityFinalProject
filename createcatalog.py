390d854194dca686b421620432c7177f33009673
Paul Lee <dpaul0720@gmail.com>
2019-06-27 20:12:56 -0500

    catalog project

---
+++ createcatalog.py
@@ -0,0 +1,87 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Item, Category, User
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+User1 = User(name="Test Account", email="testaccount@gmail.com")
+session.add(User1)
+session.commit()
+
+
+category1 = Category(user_id=1, name="RPG")
+
+session.add(category1)
+session.commit()
+
+item1 = Item(user_id=1, name="Pokemon", description="Pokemon is a series of \
+    video games developed by Game Freak and published by Nintendo and The \
+    Pokemon Company as part of the Pokemon media franchise.",
+             category=category1)
+
+
+session.add(item1)
+session.commit()
+
+item2 = Item(user_id=1, name="Skyrim", description="The Elder Scrolls V: \
+    Skyrim is an action role-playing video game developed by Bethesda Game \
+        Studios and published by Bethesda Softworks. It is the fifth main \
+            installment in The Elder Scrolls series.", category=category1)
+
+
+session.add(item2)
+session.commit()
+
+
+category2 = Category(user_id=1, name="FPS")
+
+session.add(category2)
+session.commit()
+
+item1 = Item(user_id=1, name="Counter Strike", description="Counter-Strike \
+    (CS) is a series of multiplayer first-person shooter video games, in \
+        which teams of terrorists battle to perpetrate an act of terror \
+            (bombing, hostage-taking, assassination) and counter-terrorists \
+                try to prevent it (bomb defusal, hostage rescue).",
+             category=category2)
+
+
+session.add(item1)
+session.commit()
+
+item2 = Item(user_id=1, name="Battlefield", description="Battlefield is a \
+    series of first-person shooter video games that started out on Microsoft \
+        Windows and OS X with Battlefield 1942, which was released in 2002.",
+             category=category2)
+
+
+session.add(item2)
+session.commit()
+
+category3 = Category(user_id=1, name="RTS")
+
+session.add(category3)
+session.commit()
+
+item1 = Item(user_id=1, name="StarCraft", description="StarCraft is a \
+    military science fiction media franchise, created by Chris Metzen \
+        and James Phinney and owned by Blizzard Entertainment.",
+             category=category3)
+
+
+session.add(item1)
+session.commit()
+
+item2 = Item(user_id=1, name="Age of Empires", description="Age of Empires\
+     is a series of historical real-time strategy video games, originally \
+         developed by Ensemble Studios and published by Xbox Game Studios.",
+             category=category3)
+
+
+session.add(item2)
+session.commit()
---
+++ database_setup.py
@@ -0,0 +1,65 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'email': self.email,
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category, backref=backref("items",
+                                                      cascade="all, \
+                                                          delete-orphan"))
+    user_id = Column(Integer, ForeignKey('user.id'),)
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+        }
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.create_all(engine)
